c03049764f1ff68cefe98c25703227bde7871060
ozanbote <ozanbote@windowslive.com>
2017-05-20 21:16:36 +0300

    4.4.9 -> Updated Version
    4.4.9 -> Updated Version

--- Internal/Libraries/ViewObjects/Grids/InternalDBGrid.php
+++ Internal/Libraries/ViewObjects/Grids/InternalDBGrid.php
@@ -70,7 +70,7 @@ class InternalDBGrid extends Abstracts\GridAbstract
     // @var string
     //
     //--------------------------------------------------------------------------------------------------------
-    protected $processColumn = 'ID';
+    protected $processColumn = 'id';
 
     //--------------------------------------------------------------------------------------------------------
     // Confirm
@@ -346,7 +346,7 @@ class InternalDBGrid extends Abstracts\GridAbstract
         //
         //----------------------------------------------------------------------------------------------------
         $get          = DB::get($this->table);
-        $columns      = $get->columns();
+        $columns      = array_unique($get->columns());
         $result       = $get->resultArray();
         $countColumns = count($columns);
 
@@ -553,20 +553,9 @@ class InternalDBGrid extends Abstracts\GridAbstract
                 $hiddenJoins = Form::hidden('joinsId', $this->_encode($joinsData));
             }
 
-            $table  = '<tr><td>'.($key + 1).'</td>';
-
-            foreach( $value as $val )
-            {
-                $table .= '<td>'.($val ?? NULL).'</td>';
-            }
-
-            if( $countColumns > ($countValue = count($value))) for( $i = 1; $i <= $countColumns - $countValue; $i++ )
-            {
-                $table .= '<td></td>';
-            }
-
-            $table .= '<td align="right">'.
-
+            $table .= '<tr><td>'.($key + 1).'</td><td>'.
+                    implode('</td><td>', Arrays::force($value, function($data){ return \Limiter::word((string) $data, 20);})).
+                    '</td><td align="right">'.
                     Form::action(CURRENT_CFPATH.( URI::get('page') ? '/page/'.URI::get('page') : NULL).'/column/'.suffix($hiddenValue).'process/edit')
                         ->open('editButtonForm').
                     $hiddenId.
@@ -743,7 +732,7 @@ class InternalDBGrid extends Abstracts\GridAbstract
 
         if( ! empty($this->joins) )
         {
-            $select = Arrays::addFirst($this->select, $this->table.'.'.$this->processColumn.' as ID');
+            $select = Arrays::addFirst($this->select, $this->table.'.'.$this->processColumn.' as id');
         }
 
         DB::select(...$select);
@@ -959,8 +948,7 @@ class InternalDBGrid extends Abstracts\GridAbstract
                 $this->joinTables[$currentTable] = $currentColumn;
             }
 
-            $this->joins[1] = $this->joins[0];
-            $this->joins[0] = $this->table.'.'.$this->processColumn;
+            $this->joins = Arrays::order(array_unique($this->joins));
         }
     }
 }
