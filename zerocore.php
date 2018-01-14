e66b27c8c110437e5cf7cb9ae2569c9100097280
Ozan UYKUN <znframework@gmail.com>
2018-01-14 23:08:45 +0300

    Updated Version

--- zerocore.php
+++ zerocore.php
@@ -31,7 +31,7 @@ require REQUIREMENTS_DIR . 'Autoloader.php';
 |
 */
 
-Autoloader::defines('5.5.35', 'Nikola Tesla');
+Autoloader::defines('5.5.36', 'Nikola Tesla');
 
 /*
 |--------------------------------------------------------------------------
