acf21a6e7af89e48b46fe75af954ebf6b04d0108
Ozan UYKUN <znframework@gmail.com>
2018-01-27 15:31:00 +0300

    Updated Version

---
+++ Projects/Devtools/Views/versions-notes.wizard.php
@@ -0,0 +1,59 @@
+<!-- Page Heading -->
+<div class="row">
+    <div class="col-lg-12">
+        <h1 class="page-header">
+            {{LANG['versionNotes']}} <small> {{LANG['overview']}}</small>
+        </h1>
+
+    </div>
+</div>
+<div class="row">
+
+    <div class="col-lg-12">
+
+    @foreach( $znframework as $key => $version ):
+        {[
+            if( ! Session::select($version->name) )
+            {
+                $detail = \Restful::useragent(true)->get($version->commit->url);
+
+                Session::insert($version->name, $detail);
+            }
+            else
+            {
+                $detail = Session::select($version->name);
+            }   
+        ]}
+        @if( $detail->commit ?? NULL ):
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 style="cursor:pointer" data-toggle="collapse" data-target="/#id{{$key}}" class="panel-title">
+                <i class="fa fa-book fa-fw"></i>
+                {{$version->name}}
+                <span><i class="fa fa-angle-down fa-fw"></i></span>
+            </h3>
+        </div>
+        <div id="id{{$key}}" class="collapse panel-body">
+            <div class="list-group">
+                {{$detail->commit->message}}
+                <p>
+                <a target="_blank" href="{{$detail->html_url}}">&raquo; @LANG['detail']:</a>
+                </p>
+            </div>
+        </div>
+    </div>
+        @endif:
+    @endforeach:
+    </div>
+
+</div>
+
+@@Form::close():
+
+@Import::plugin(array
+(
+	'Dashboard/highlight/styles/agate.css',
+	'Dashboard/highlight/highlight.pack.js'
+)):
+
+<script>hljs.initHighlightingOnLoad();</script>
