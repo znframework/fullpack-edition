0ca1b65a7a8015e8d39f329c0008f8ec041a7961
ozanuykun <YOU@EXAMPLE.COM>
2018-05-21 23:48:02 +0200

    Updated devtools.

--- Projects/Devtools/Models/Initialize/Database.php
+++ Projects/Devtools/Models/Initialize/Database.php
@@ -1,4 +1,13 @@
 <?php namespace Initialize;
+/**
+ * ZN PHP Web Framework
+ * 
+ * "Simplicity is the ultimate sophistication." ~ Da Vinci
+ * 
+ * @package ZN
+ * @license MIT [http://opensource.org/licenses/MIT]
+ * @author  Ozan UYKUN [ozan@znframework.com]
+ */
 
 use ZN\Config;
 use ZN\Base;
