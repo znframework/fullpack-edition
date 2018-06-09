628132556a3a25f8a6440917851aa46139bda2ce
ozanuykun <YOU@EXAMPLE.COM>
2018-06-09 22:23:03 +0300

    5.7.6.4: Updated version.

--- Internal/package-zerocore/Inclusion/Project/ViewTrait.php
+++ Internal/package-zerocore/Inclusion/Project/ViewTrait.php
@@ -21,13 +21,23 @@ trait ViewTrait
     public static $data = [];
 
     /**
-     * Usable methods
+     * Usable view methods
      * 
      * @var array
      */
-    public static $usableMethods =
+    protected static $usableViewMethods =
     [
-        'view', 'script', 'style', 'font', 'template', 'page', 'something', 'theme', 'plugin'
+        'page', 'view', 'cview', 'template', 'something'
+    ];
+
+    /**
+     * Usable resources methods
+     * 
+     * @var array
+     */
+    protected static $usableResourcesMethods =
+    [
+        'script', 'style', 'font', 'theme', 'plugin'
     ];
 
     /**
@@ -76,36 +86,73 @@ trait ViewTrait
      */
     protected static function call($method, $parameters)
     {
-        if( isset(self::$data[$method]) && empty($parameters) )
+        # If the invoked method does not contain a parameter, it returns the current value.
+        if( empty($parameters) )
         {
-            return self::$data[$method];
+            return self::$data[$method] ?? false;
         }
 
-        if( is_scalar($parameters[0]) )
+        # If the parameter of the invoked method contains a measurable value, 
+        # a special operation is executed.
+        if( self::isImportMethod($parameters[0], $resolve) )
         {
-            $ex = explode(':', $parameters[0]);
+            # The other parameters are used like the parametric array of the import method.
+            $getImportMethodParameters = self::getImportMethodParameters($parameters);
 
-            if( in_array($met = $ex[0], self::$usableMethods) )
+            # If the inclusion method is one of the following 4 methods;
+            # [cview|page|view|something|template]
+            # If no data is sent to the import method, 
+            # the second parameter of the method is set to NULL.
+            if( in_array($getImportMethod = $resolve['method'], self::$usableViewMethods) && ! is_array($getImportMethodParameters[0]) )
             {
-                $pr = Singleton::class('ZN\DataTypes\Collection')->data($parameters)
-                                                                 ->removeFirst()
-                                                                 ->addLast(true)
-                                                                 ->get();
-
-                if( strstr('page|view|something|template', $met) && ! is_array($pr[0]) )
-                {
-                    array_unshift($pr, NULL);
-                }
-   
-                self::$data[$method] = $value = Singleton::class('ZN\Import')->$met($ex[1] ?? NULL, ...$pr);
+                array_unshift($getImportMethodParameters, NULL);
             }
+
+            # Getting the contents of the included file.
+            self::$data[$method] = $isImportMethodContent = self::getImportMethod($getImportMethod, $resolve['parameter'], $getImportMethodParameters);
         }
 
-        if( empty($value) )
+        # If the included file is not valid or has no content, 
+        # the parameter is assumed to be a normal value.
+        if( empty($isImportMethodContent) )
         {
-            self::$data[$method] = $parameters[0] ?? false;
+            self::$data[$method] = $parameters[0];
         }
 
+        # Returning NULL resumes object transfer. The return value is assumed to be $this.
         return NULL;
+    }
+
+    /**
+     * Protected is import method
+     */
+    protected static function isImportMethod($parameter, &$match)
+    {
+        return is_scalar($parameter) && preg_match
+        (
+            '/^(?<method>' . implode('|', self::$usableViewMethods + self::$usableResourcesMethods) . ')\:(?<parameter>.*)/', 
+            $parameter, 
+            $match
+        );
+    }
+
+    /**
+     * Protected get import method
+     */
+    protected static function getImportMethod($getImportMethod, $firstParameter, $getOtherParameters)
+    {
+        return Singleton::class('ZN\Import')->$getImportMethod($firstParameter, ...$getOtherParameters);
+    }
+
+    /**
+     * Protected get import method parameters
+     */
+    protected static function getImportMethodParameters($parameters)
+    {
+        return Singleton::class('ZN\DataTypes\Collection')
+                        ->data($parameters)
+                        ->removeFirst()
+                        ->addLast(true)
+                        ->get();
     }
 }
