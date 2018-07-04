34db4e87f36f8f05a3a589b02bae99a3188e1a25
Ozan Uykun <znframework@gmail.com>
2018-07-04 16:10:50 +0300

    Modified protected ZN\Expcetions::getTemplateWizardErrorData() method.

--- Projects/Frontend/Views/Home/main.wizard.php
+++ Projects/Frontend/Views/Home/main.wizard.php
@@ -2,13 +2,13 @@
     <div id="content">
         <div id="title">{{$pageTitle}} <span class="blue-color">{{ZN_VERSION}}</span> is dedicated to <span class="blue-color">{{ZN_DEDICATE}}</span></div>
         <div id="sup-title-2" class="grey-color">
-            n&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            n
         </div>
         <div id="sub-title" class="grey-color">
             Arf(q) = <span class="sum">&sum;</span> q(a<sub>i</sub>)q(b<sub>i</sub>) &isin; &Zeta;<sub>2</sub>
         </div>
         <div id="sub-title-2" class="grey-color">
-            i=1&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            i=1
         </div>
         <div id="sub-title-3" class="grey-color">
             a<sub>i</sub>, b<sub>i</sub> i = 1, 2, 3, ..., n
