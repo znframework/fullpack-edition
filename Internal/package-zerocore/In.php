0922c6409450e2b6f15e66d5c9687c5c029d9e91
Ozan Uykun <znframework@gmail.com>
2018-07-24 20:27:31 +0300

    Changed ZN\In::defaultProjectKey() method.

--- Internal/package-zerocore/In.php
+++ Internal/package-zerocore/In.php
@@ -107,7 +107,7 @@ class In
      */
     public static function defaultProjectKey(String $fix = NULL) : String
     {
-        return md5(Request::getBaseURL(strtolower(CONTAINER_PROJECT)) . $fix);
+        return md5('http://' . Base::host(strtolower(CONTAINER_PROJECT)) . $fix);
     }
 
     /**
