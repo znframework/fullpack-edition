05b93d107650ba7a29f38b5a78f144edcba389e5
Ozan Uykun <znframework@gmail.com>
2018-07-28 20:05:10 +0300

    Fixed Validator::required() method.

--- Internal/package-hypertext/ViewCommonTrait.php
+++ Internal/package-hypertext/ViewCommonTrait.php
@@ -100,7 +100,7 @@ trait ViewCommonTrait
         {
             if( in_array($method, $this->elements['input']) )
             {
-                return $this->_input($parameters[0], $parameters[1] ?? NULL, $parameters[2] ?? [], $realMethod);
+                return $this->_input($parameters[0] ?? '', $parameters[1] ?? '', $parameters[2] ?? [], $realMethod);
             }
         }
 
