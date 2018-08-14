6a113f4c7866e62bff84132292fa509d9b2f6461
Ozan Uykun <znframework@gmail.com>
2018-08-14 20:20:00 +0300

    Modified Form::serializer() & Form::trigger() methods.

--- Internal/package-hypertext/Resources/Ajax/serializer.wizard.php
+++ Internal/package-hypertext/Resources/Ajax/serializer.wizard.php
@@ -1,22 +1,25 @@
 <script>
-
 function {{$serializerFunction}}(element)
 {
-    $.ajax
-    ({
-        url  : '{{ URL::site($serializerUrl) }}', 
-        type : 'post', 
-        data : $(element).closest('form').serialize(),
-        {{ $serializerProperties}}
-        success:function(data)
-        {
-            @if( is_string($serializerSelector) )
-            $('{{$serializerSelector}}').html(data);
-            @elseif( is_callable($serializerSelector) )
-            {{ $serializerSelector() }}
-            @endif
-        }
-    });
-}
+    var form = $(element).closest('form');
 
+    if ( form.is(":valid") ) 
+    {
+        $.ajax
+        ({
+            url  : '{{ URL::site($serializerUrl) }}', 
+            type : 'post', 
+            data : form.serialize(),
+            {{ $serializerProperties}}
+            success:function(data)
+            {
+                @if( is_string($serializerSelector) )
+                $('{{$serializerSelector}}').html(data);
+                @elseif( is_callable($serializerSelector) )
+                {{ $serializerSelector() }}
+                @endif
+            }
+        });
+    } 
+}
 </script>
