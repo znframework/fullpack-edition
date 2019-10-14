1c7209ff488222d93ee3532a739be4fff2b5570f
znframework <ozanbote@gmail.com>
2019-10-14 14:19:14 +0300

    Added Import::wscript(string $file) method.

--- Internal/package-zerocore/Inclusion/Style.php
+++ Internal/package-zerocore/Inclusion/Style.php
@@ -12,6 +12,35 @@
 class Style extends BootstrapExtends
 {
     /**
+     * Dynamic wizard script
+     * 
+     * @param string $script
+     * 
+     * @return string
+     */
+    public function wizard(String $script)
+    {
+        if( isset($script[0]) && $script[0] === '/' )
+        {
+            $script = CURRENT_CONTROLLER . $script;
+        }
+
+        $currentDirectory = pathinfo($script . '.php', PATHINFO_DIRNAME) . '/';
+        $currentFile      = pathinfo($script . '.php', PATHINFO_FILENAME);
+
+        $themeDirectory = defined('CURRENT_THEME_DIR') ? CURRENT_THEME_DIR : THEMES_DIR;
+
+        if( ! is_dir($fullPath = $themeDirectory . $currentDirectory) )
+        {
+            mkdir($fullPath, 0755, true);
+        }
+
+        file_put_contents($path = $fullPath . \ZN\Filesystem::removeExtension($currentFile) . '.wizard.php', View::use($script, NULL, true));
+
+        return self::tag($path);
+    }
+    
+    /**
      * HTML Element
      * 
      * @param string $src = NULL
