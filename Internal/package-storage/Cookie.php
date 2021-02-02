aede000eb19166705698677deeaedb98df788e2f
ozanuykun <ozanbote@gmail.com>
2021-02-02 13:25:49 +0300

    Fixed Storage\Cookie::selectAll method.

--- Internal/package-storage/Cookie.php
+++ Internal/package-storage/Cookie.php
@@ -200,7 +200,7 @@ class Cookie implements CookieInterface, StorageInterface
         }
         else
         {
-            return false;
+            return [];
         }
     }
 
--- zeroneed.php
+++ zeroneed.php
@@ -32,4 +32,4 @@ require __DIR__ . '/Internal/autoload.php';
 |
 */
 
-ZN\ZN::run('FE', '8.2.4.1', 'Mustafa Kemal Atatürk');
+ZN\ZN::run('FE', '8.2.4.2', 'Mustafa Kemal Atatürk');
