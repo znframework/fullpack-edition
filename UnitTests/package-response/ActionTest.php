7fb1cee0d2beb446408244ee9391b55d74a4f4df
ozanuykun <ozanbote@gmail.com>
2021-02-03 22:57:06 +0300

    Covered package-response package.

--- UnitTests/package-response/ActionTest.php
+++ UnitTests/package-response/ActionTest.php
@@ -1,6 +1,5 @@
 <?php namespace ZN\Response;
 
-use Refresh;
 use Redirect;
 
 class ActionTest extends \PHPUnit\Framework\TestCase
@@ -8,15 +7,10 @@ class ActionTest extends \PHPUnit\Framework\TestCase
     public function testAction()
     {
         Redirect::exit(false)
-                ->time(0) 
+                ->time(0) # same wait
+                ->wait(0)
+                ->data(['example' => 'Example']) # same insert
                 ->insert(['example' => 'Example'])
                 ->action('profile');
-    }
-
-    public function testActionWithRefreshClass()
-    {
-        Refresh::wait(0)
-               ->data(['example' => 'Example'])
-               ->action('profile');
     }
 }
