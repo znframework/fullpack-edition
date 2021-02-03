59cd21aa4826cb5404c64823a92b30e7192da7c7
ozanuykun <ozanbote@gmail.com>
2021-02-03 18:05:06 +0300

    Extended Shopping\UpdateItemTest class.

--- UnitTests/package-shopping/UpdateItemTest.php
+++ UnitTests/package-shopping/UpdateItemTest.php
@@ -20,4 +20,45 @@ class UpdateItemTest extends \PHPUnit\Framework\TestCase
 
         Cart::delete('4432222345219');
     }
+
+    public function testUpdateByArrayParameter()
+    {
+        $insert = Cart::insert
+        ([
+            'product'       => 'Banana',
+            'price'         => '10',
+            'quantity'      => 3,
+            'serial-number' => '4432222345219'
+        ]);
+
+        Cart::update(['product' => 'Banana'], ['price' => 20]);
+
+        $this->assertEquals(60, Cart::totalPrices());
+
+        Cart::delete('4432222345219');
+    }
+
+    public function testUpdateAddedUndefinedKey()
+    {
+        $insert = Cart::insert
+        ([
+            'product'       => 'Banana',
+            'price'         => '10',
+            'quantity'      => 3,
+            'serial-number' => '4432222345219'
+        ]);
+
+        Cart::update(['product' => 'Banana'], ['price' => 20, 'unknown' => '1234']);
+
+        $this->assertEquals('1234', Cart::select(['unknown' => '1234'])->unknown);
+
+        Cart::delete('4432222345219');
+    }
+
+    public function testEmptyReturnFalse()
+    {
+        Cart::deleteAll();
+
+        $this->assertFalse(Cart::update('Banana', ['price' => 20]));
+    }
 }
