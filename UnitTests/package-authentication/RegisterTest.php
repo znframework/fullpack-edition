cf761aafc3e816d2bea2e4d86da502a80ad39ee7
ozanuykun <ozanbote@gmail.com>
2021-02-03 11:53:59 +0300

    Extended Authentication\RegisterTest class.

--- UnitTests/package-authentication/RegisterTest.php
+++ UnitTests/package-authentication/RegisterTest.php
@@ -2,6 +2,8 @@
 
 use DB;
 use User;
+use Config;
+use DBForge;
 
 class RegisterTest extends AuthenticationExtends
 {
@@ -46,5 +48,59 @@ class RegisterTest extends AuthenticationExtends
         $this->assertEquals('robot@znframework.com', User::data()->username);
 
         DB::where('username', 'robot@znframework.com')->delete('users');
+    }
+
+    public function testJoinColumn()
+    {
+        DB::where('username', 'robot@znframework.com')->delete('users');
+
+        DBForge::dropTable('addresses');
+
+        DBForge::createTable('addresses',
+        [
+            'username' => [DB::varchar(255)],
+            'address'  => [DB::varchar(255)]
+        ]);
+
+        Config::set('Auth', 
+        [
+            'joining' =>
+            [
+                'column' => 'username',
+                'tables' => ['addresses' => 'username']
+            ]
+        ]);
+
+        User::register
+        ([
+            'users' => 
+            [
+                'username' => 'robot@znframework.com',
+                'password' => '1234'
+            ],
+            'addresses' => 
+            [
+                'address' => 'London'
+            ]
+        ]);
+
+        User::login('robot@znframework.com', '1234');
+
+        $data = User::data('addresses');
+
+        $this->assertEquals('London', $data->address);
+
+        DB::where('username', 'robot@znframework.com')->delete('users');
+
+        DBForge::dropTable('addresses');
+
+        Config::set('Auth', 
+        [
+            'joining' =>
+            [
+                'column' => '',
+                'tables' => []
+            ]
+        ]);
     }
 }
