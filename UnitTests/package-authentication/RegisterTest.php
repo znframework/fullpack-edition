99eb4da765551c914f350373b32963c2fa4d4e4a
ozanuykun <ozanbote@gmail.com>
2021-02-03 12:35:54 +0300

    Extended Authentication\RegisterTest class.

--- UnitTests/package-authentication/RegisterTest.php
+++ UnitTests/package-authentication/RegisterTest.php
@@ -36,6 +36,20 @@ class RegisterTest extends AuthenticationExtends
         $this->assertEquals('robot@znframework.com', User::data()->username);
     }
 
+    public function testStandartWithAutoLoginRedirect()
+    {
+        return false;
+        
+        DB::where('username', 'robot@znframework.com')->delete('users');
+
+        User::register
+        ([
+            'username' => 'robot@znframework.com',
+            'password' => '1234'
+
+        ], 'redirect/link');
+    }
+
     public function testStandartWithWithOptionalMethodAutoLogin()
     {
         DB::where('username', 'robot@znframework.com')->delete('users');
@@ -112,5 +126,137 @@ class RegisterTest extends AuthenticationExtends
         ]);
 
         $this->assertEquals('Unknown error!', User::error());
+    }
+
+    public function testActivationColumnActivationReturnLinkNotFoundException()
+    {
+        DB::where('username', 'robot')->delete('accounts');
+
+        DBForge::dropTable('accounts');
+
+        DBForge::createTable('accounts',
+        [
+            'username'      => [DB::varchar(255)],
+            'password'      => [DB::varchar(255)],
+            'email'         => [DB::varchar(255)],
+            'activation'    => [DB::varchar(255)]
+        ]);
+
+        Config::set('Auth', 
+        [
+            'matching'  =>
+            [
+                'table'   => 'accounts',
+                'columns' =>
+                [
+                    'username'     => 'username',
+                    'password'     => 'password', 
+                    'email'        => 'email',              
+                    'active'       => 'active',      
+                    'banned'       => 'banned',       
+                    'activation'   => 'activation',     
+                    'verification' => '',   
+                    'otherLogin'   => ['phone']         
+                ]
+            ]
+        ]);
+            
+        try
+        {
+            User::register
+            ([
+                'username' => 'robot',
+                'password' => '1234',
+                'email'    => 'robot@znframework.com'
+            ]);
+        }
+        catch( Exception\ActivationReturnLinkNotFoundException $e )
+        {
+            $this->assertEquals('The return link must be specified for the activation process!', $e->getMessage());
+        }
+
+        Config::set('Auth', 
+        [
+            'matching'  =>
+            [
+                'table'   => 'users',
+                'columns' =>
+                [
+                    'username'     => 'username',
+                    'password'     => 'password', 
+                    'email'        => '',              
+                    'active'       => 'active',      
+                    'banned'       => 'banned',       
+                    'activation'   => '',     
+                    'verification' => '',   
+                    'otherLogin'   => ['phone']         
+                ]
+            ]
+        ]);
+    }
+
+    public function testActivationColumn()
+    {
+        DB::where('username', 'robot@znframework.com')->delete('accounts');
+
+        DBForge::dropTable('accounts');
+
+        DBForge::createTable('accounts',
+        [
+            'username'      => [DB::varchar(255)],
+            'password'      => [DB::varchar(255)],
+            'activation'    => [DB::varchar(255)]
+        ]);
+
+        Config::set('Auth', 
+        [
+            'matching'  =>
+            [
+                'table'   => 'accounts',
+                'columns' =>
+                [
+                    'username'     => 'username',
+                    'password'     => 'password', 
+                    'email'        => '',              
+                    'active'       => 'active',      
+                    'banned'       => 'banned',       
+                    'activation'   => 'activation',     
+                    'verification' => '',   
+                    'otherLogin'   => ['phone']         
+                ]
+            ]
+        ]);
+            
+        try
+        {
+            User::register
+            ([
+                'username' => 'robot@znframework.com',
+                'password' => '1234'
+            ], false, 'return/link');
+        }
+        catch( \Exception $e )
+        {
+            $this->assertStringContainsString('is an invalid email address!', $e->getMessage());
+        }
+
+        Config::set('Auth', 
+        [
+            'matching'  =>
+            [
+                'table'   => 'users',
+                'columns' =>
+                [
+                    'username'     => 'username',
+                    'password'     => 'password', 
+                    'email'        => '',              
+                    'active'       => 'active',      
+                    'banned'       => 'banned',       
+                    'activation'   => '',     
+                    'verification' => '',   
+                    'otherLogin'   => ['phone']         
+                ]
+            ]
+        ]);
     }
 }
