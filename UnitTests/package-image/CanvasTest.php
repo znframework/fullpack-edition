665460cec4cbddfc08fb2cf34960ef746ba81c15
ozanuykun <ozanbote@gmail.com>
2021-02-04 19:49:21 +0300

    Covered package-image package.

--- UnitTests/package-image/CanvasTest.php
+++ UnitTests/package-image/CanvasTest.php
@@ -6,6 +6,6 @@ class CanvasTest extends Test\GDExtends
 {
     public function testCanvasReal()
     {
-        $this->assertIsObject(GD::canvas(300, 300, 'white', true)->generate('png'));
+        GD::canvas(300, 300, 'white', true)->generate('png');
     }
 }
