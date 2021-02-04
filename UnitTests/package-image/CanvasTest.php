078040f17f927332d9a3edbae66a4e81d10685cb
ozanuykun <ozanbote@gmail.com>
2021-02-04 19:50:56 +0300

    Covered package-image package.

--- UnitTests/package-image/CanvasTest.php
+++ UnitTests/package-image/CanvasTest.php
@@ -1,11 +1,17 @@
 <?php namespace ZN\Image;
 
 use GD;
+use Buffer;
 
 class CanvasTest extends Test\GDExtends
 {
     public function testCanvasReal()
     {
-        GD::canvas(300, 300, 'white', true)->generate('png');
+        $file = Buffer::callback(function()
+        {
+            GD::canvas(300, 300, 'white', true)->generate('png');
+        });
+       
+        $this->assertIsString($file);
     }
 }
