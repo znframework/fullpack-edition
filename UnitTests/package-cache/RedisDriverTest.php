a31f6a73727edff379eac101a66ef20fee15cc78
ozanuykun <ozanbote@gmail.com>
2021-02-08 23:46:35 +0300

    phputil.yml

--- UnitTests/package-cache/RedisDriverTest.php
+++ UnitTests/package-cache/RedisDriverTest.php
@@ -17,4 +17,108 @@ class RedisDriverTest extends CacheExtends
             echo $e->getMessage();
         }      
     }
+
+    public function testDelete()
+    {
+        try
+        {
+            $redis = $this->redis();
+
+            $redis->insert('example', 1);
+
+            $redis->delete('example');
+    
+            $this->assertEmpty($redis->select('example'));
+        }
+        catch( \Exception $e )
+        {
+            echo $e->getMessage();
+        }      
+    }
+
+    public function testDecrement()
+    {
+        try
+        {
+            $redis = $this->redis();
+
+            $redis->insert('a', 2);
+
+            $redis->decrement('a');
+    
+            $this->assertEquals(1, $redis->select('a'));
+        }
+        catch( \Exception $e )
+        {
+            echo $e->getMessage();
+        }   
+    }
+
+    public function testIncrement()
+    {
+        try
+        {
+            $redis = $this->redis();
+
+            $redis->insert('a', 1);
+
+            $redis->increment('a');
+    
+            $this->assertEquals(2, $redis->select('a'));
+        }
+        catch( \Exception $e )
+        {
+            echo $e->getMessage();
+        }   
+    }
+
+    public function testInfo()
+    {
+        try
+        {
+            $redis = $this->redis();
+
+            $redis->insert('a', 1);
+    
+            $redis->info();
+        }
+        catch( \Exception $e )
+        {
+            echo $e->getMessage();
+        } 
+    }
+
+    public function testClean()
+    {
+        try
+        {
+            $redis = $this->redis();
+
+            $redis->insert('a', 1);
+    
+            $redis->clean();
+
+            $this->assertEmpty($redis->select('a'));
+        }
+        catch( \Exception $e )
+        {
+            echo $e->getMessage();
+        } 
+    }
+
+    public function testGetMetaData()
+    {
+        try
+        {
+            $redis = $this->redis();
+
+            $redis->insert('a', 1);
+    
+            $this->assertIsArray($redis->getMetaData('a'));
+        }
+        catch( \Exception $e )
+        {
+            echo $e->getMessage();
+        } 
+    }
 }
