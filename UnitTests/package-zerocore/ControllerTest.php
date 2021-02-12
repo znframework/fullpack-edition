f193e8e0870f1203f44867a2e4680c768871400e
ozanuykun <ozanbote@gmail.com>
2021-02-12 19:36:12 +0300

    Covered package-zerocore package.

--- UnitTests/package-zerocore/ControllerTest.php
+++ UnitTests/package-zerocore/ControllerTest.php
@@ -6,9 +6,9 @@ class ControllerTest extends ZerocoreExtends
 {
     public function testGet()
     {
-        $this->assertEquals('eca07335a33c5aeb5e1bc7c98b4b9d80', $this->controllerMock->encode->type('param'));
-        
         $this->assertEquals('Example', $this->controllerMock->run());
+        
+        $this->assertEquals('eca07335a33c5aeb5e1bc7c98b4b9d80', $this->controllerMock->encode->type('param'));
 
         $this->assertNull($this->controllerMock->restart());
     }
