2bbb1bf78ea250ea2c746f37d9fab05936bbced5
ozanuykun <ozanbote@gmail.com>
2021-02-12 21:19:16 +0300

    Covered package-zerocore package.

--- UnitTests/package-zerocore/HtaccessTest.php
+++ UnitTests/package-zerocore/HtaccessTest.php
@@ -1,11 +1,17 @@
 <?php namespace ZN;
 
+use File;
+
 class HtaccessTest extends ZerocoreExtends
 {
     public function testCreate()
     {
         define('HTACCESS_CONFIG', Config::htaccess());
+
+        $this->assertFalse(Htaccess::create());
         
-        Htaccess::create();
+        File::delete('.htaccess');
+
+        $this->assertTrue(Htaccess::create());
     }
 }
