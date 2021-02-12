d69542e742d27d0cf2aa7133ebc3a8ac0c01ff8c
ozanuykun <ozanbote@gmail.com>
2021-02-12 20:30:52 +0300

    Covered package-zerocore package.

---
+++ UnitTests/package-zerocore/IniTest.php
@@ -0,0 +1,39 @@
+<?php namespace ZN;
+
+class IniTest extends ZerocoreExtends
+{
+    public function testCall()
+    {
+        $this->assertNull(Ini::postMaxSize('10M'));
+        $this->assertEquals('40M', Ini::postMaxSize());
+
+        try
+        {
+            Ini::unknown(5);
+        }
+        catch( \Exception $e )
+        {
+            $this->assertEquals('The [unknown] method is not a valid ini configuration!', $e->getMessage());
+        }
+    }
+
+    public function testSet()
+    {
+        $this->assertIsString(Ini::set('post_max_size', '10M'));
+    }
+
+    public function testGet()
+    {
+        $this->assertIsString(Ini::get('post_max_size'));
+    }
+
+    public function testRestore()
+    {
+        $this->assertNull(Ini::restore('post_max_size'));
+    }
+
+    public function testAll()
+    {
+        $this->assertIsArray(Ini::getAll());
+    }
+}
