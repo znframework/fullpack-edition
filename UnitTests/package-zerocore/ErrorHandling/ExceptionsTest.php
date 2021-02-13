164d1d4bb01c912265bf2ee8d544add677725336
ozanuykun <ozanbote@gmail.com>
2021-02-13 19:22:07 +0300

    Covered package-zerocore package.

--- UnitTests/package-zerocore/ErrorHandling/ExceptionsTest.php
+++ UnitTests/package-zerocore/ErrorHandling/ExceptionsTest.php
@@ -1,5 +1,6 @@
 <?php namespace ZN\ErrorHandling;
 
+use File;
 use Buffer;
 
 class ExceptionsTest extends \PHPUnit\Framework\TestCase
@@ -32,5 +33,25 @@ class ExceptionsTest extends \PHPUnit\Framework\TestCase
     public function testRestore()
     {  
         $this->assertIsBool(Exceptions::restore());
+    }
+
+    public function testTable()
+    {  
+        try
+        {
+            File::reglace('unknownfile', 'abc', 'xyzz');
+        }
+        catch( \Exception $e )
+        {
+            $result = Buffer::callback(function() use($e)
+            { 
+                ob_start();
+                        
+                Exceptions::table($e);
+            }); 
+
+            $this->assertIsString($result);
+        }
+       
     }
 }
