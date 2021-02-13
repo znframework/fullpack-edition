823dc92072a9c50cfa8aa15b5c547c98636cccfc
ozanuykun <ozanbote@gmail.com>
2021-02-13 14:09:43 +0300

    Covered package-zerocore package.

--- Internal/package-zerocore/Command.php
+++ Internal/package-zerocore/Command.php
@@ -23,7 +23,6 @@ class Command extends Base
         # If the operation is executed via console, the code flow is not continue.  
         if( ! defined('CONSOLE_ENABLED') )
         {
-           
             throw new Exception('Commands', 'canNotCommandClass'); // @codeCoverageIgnore
         }
     }
