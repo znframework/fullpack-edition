78b36a8ba595c565e4355b032fe65f71a9b46789
ozanuykun <ozanbote@gmail.com>
2021-03-14 11:30:31 +0300

    Covered Filesystem\Forge::createZip method.

--- Internal/package-filesystem/Forge.php
+++ Internal/package-filesystem/Forge.php
@@ -156,7 +156,7 @@ class Forge
 
         if( file_exists($zipPath) )
         {
-            unlink($zipPath);
+            unlink($zipPath); // @codeCoverageIgnore
         }
 
         if( ! is_dir($pathDirName = Info::pathInfo($path, 'dirname')) )
