7f83f7b2311091b93a3436f275c4733ed44c6c61
Remzi ÖNEL <remzionel@outlook.com.tr>
2022-04-28 23:28:33 +0300

    Fix "Undefined offset: 0" error
    Is the fix to "Message: Satır:1913, Dosya:/..../Internal/package-database/DB.php, Hata:Undefined offset: 0" error.

--- Internal/package-database/DB.php
+++ Internal/package-database/DB.php
@@ -1909,7 +1909,7 @@ class DB extends Connection
         {
             if( $printable === true )
             {
-                return current((array) $result[0] ?? []);
+                return isset($result[0]) ? (array) current($result[0]) : [];
             }
 
             return isset($result[$printable]) ? (object) $result[$printable] : false;
