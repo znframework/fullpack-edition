eaab4373ac3f5c468d09f6da21a9500341cb252f
ozanuykun <ozanbote@gmail.com>
2022-12-30 01:01:59 +0300

    Remove revolving from Pagination library.

--- Internal/package-pagination/Paginator.php
+++ Internal/package-pagination/Paginator.php
@@ -13,12 +13,9 @@ use ZN\Base;
 use ZN\Config;
 use ZN\Request\URL;
 use ZN\Request\URI;
-use ZN\Ability\Revolving;
 
 class Paginator implements PaginatorInterface
 {
-    use Revolving;
-
     /**
      * Keep settings
      * 
